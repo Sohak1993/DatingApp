05578578354c7f48d203c2da3fc0baca0a7431ad
Sohak1993 <mehdy.maessckal@gmail.com>
2023-01-18 18:20:16 +0100

    Ajoutez des fichiers projet.

---
+++ DAL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DAL
+{
+    public class DataContext : DbContext
+    {
+        //public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("./DatingApp/appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("default"));
+        }
+    }
+}
